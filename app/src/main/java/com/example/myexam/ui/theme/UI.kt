381d4bd1a5d6c78d30aed58ab38f9ce7d4eb6df3
Marcos <marcosfernandezstudent@gmail.com>
2023-10-25 10:32:17 +0200

    Finish

--- app/src/main/java/com/example/myexam/ui/theme/UI.kt
+++ app/src/main/java/com/example/myexam/ui/theme/UI.kt
@@ -22,10 +22,12 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myexam.Nombre
@@ -167,11 +169,7 @@ fun Greeting(miModel: Model) {
 
         Textos(miModel = miModel)
 
-
-
-
-
-
-
 }
+
+
 
